61ddb342c0c626dcb140c102b7e7de4374a280d5
Lucas Chang <lucaskchang@gmail.com>
2019-08-05 19:58:34 -0700

    Change URL

--- scripts/sets.js
+++ scripts/sets.js
@@ -1,3 +1,5 @@
+
+
 //Populates all of the sets for the seacrh bar
 $(document).ready(function() {
   var myjson = (function() {
@@ -17,5 +19,25 @@ $(document).ready(function() {
       y = i.replace(/\s+/g, '').replace("part", "")
       $(".set-search").append("<li><a style='width: auto;margin: auto' href='https://lukaja21.github.io/Quizlit/sets/" + myjson[i][0] + "/" + y.toLowerCase() + ".html'>" + i + "</a></li>")
       $(".set-search").append("<div class='dropdown-divider'></div>")
+  }
+})
+$(document).ready(function() {
+  if(window.location.href.indexOf("sets") > -1) {
+       $.ajax({
+          method: 'GET',
+          url: 'https://lukaja21.github.io/Quizlit/data/data.json',
+          dataType: 'text',
+          success: myjson => {
+            if (window.location.href.indexOf("chinese") > -1) {
+              subject = "chinese"
+            } else if (window.location.href.indexOf("chinese") > -1) {
+              subject = "science"
+            }  else if (window.location.href.indexOf("chinese") > -1) {
+              subject = "math"
+            }
+            for set in myjson:
+              if set[0] = ""
+      }
+    })
   }
 })

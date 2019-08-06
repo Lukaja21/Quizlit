23c8c04ff83199fa9f420ca9434836f14c0d7cca
Lucas Chang <lucaskchang@gmail.com>
2019-08-05 18:08:10 -0700

    Front Page loaded from Json

--- scripts/sets.js
+++ scripts/sets.js
@@ -1,21 +1,3 @@
-var y;
-//Populates sets for the sets pages
-$(document).ready(function() {
-  var SetContainerNumber = $('.set-container').length;
-  if (SetContainerNumber > 10) {
-    $(".set-container").slice(10).hide()
-    pages = Math.ceil(SetContainerNumber / 10) + 1
-    for (i = 2; i < pages; i++) { 
-      $(".pagination").append("<li class='page-item'><a class='page-link' onclick='nextpage(" + i.toString() + ");'>" + i.toString() + "</a></li>")
-    }
-    maxpage = $('.page-item').length;
-  } else {
-    $(".subject-nav").hide()
-    if (SetContainerNumber == 0) {
-      $(".subject-container").append("<h1 class='no-sets-warning'>Oops! Looks like there are no study sets for this subject yet.</h1>")
-    }
-  }
-});
 //Populates all of the sets for the seacrh bar
 $(document).ready(function() {
   var myjson = (function() {
@@ -37,29 +19,3 @@ $(document).ready(function() {
       $(".set-search").append("<div class='dropdown-divider'></div>")
   }
 })
-
-function GetSetCards(SetName) {
-  var myjson = (function() {
-    var myjson = null;
-    $.ajax({
-      'async': false,
-      'global': false,
-      'url': "https://lukaja21.github.io/Quizlit/data/data.json",
-      'dataType': "json",
-      'success': function (data) {
-          myjson = data;
-      }
-    });
-  return myjson;
-})();
-  return myjson[SetName][1]
-}
-
-$(document).ready(function(){
-  $("#myInput").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $(".dropdown-menu li").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
-  });
-});

4c5f576ad04125060ac8bbf9949e626ec28cdf3a
Lucas Chang <lucaskchang@gmail.com>
2020-03-09 19:09:42 -0700

    Settg

--- scripts/script.js
+++ scripts/script.js
@@ -5,10 +5,7 @@ var accesibilityHidden = true
 var rotation = 0;
 
 jQuery.fn.rotate = function(degrees) {
-    $(this).css({'-webkit-transform' : 'rotate('+ degrees +'deg)',
-                 '-moz-transform' : 'rotate('+ degrees +'deg)',
-                 '-ms-transform' : 'rotate('+ degrees +'deg)',
-                 'transform' : 'rotate('+ degrees +'deg)'});
+    $(this).css({'transform' : 'rotate('+ degrees +'deg)'});
     return $(this);
 };
 
